ddfee1b8b82a59a214fdb8d9f913ceaccb88962d
Usman Anwar <uanwar88@gmail.com>
2015-04-09 09:59:08 -0500

    Working on associations. Deleted Session model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :city
   belongs_to :category
-  has_many :stars, dependent: :delete
+  has_many :stars, dependent: :delete_all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :events, dependent: :delete
-  has_many :stars, dependent: :delete
+  has_many :events, dependent: :delete_all
+  has_many :stars, dependent: :delete_all
   has_many :starred_events, through: :stars, source: :event
 end
 

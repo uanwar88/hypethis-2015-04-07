9f43865bba9a270bca6753fcf81f7b5e800bcf6a
Usman Anwar <uanwar88@gmail.com>
2015-04-15 13:27:53 -0500

    Working on categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,42 +1,43 @@
-
-  scope :bw_dates_and_cat, lambda{|start_date,end_date,category_id| where("start_time >= ? and start_time <= ? and category_id = ?", start_date, end_date, category_id)}
+class CategoriesController < ApplicationController
+  scope :between_dates, lambda{|start_date,end_date| where("start_time >= ? and start_time <= ?", start_date, end_date) }
+  scope :category, ->(category_id) { where(category_id:  category_id) }
 
   def index
   end
 
   def show
     time = DateTime.now
-    @events = City.find_by_city(params[:city]).events.bw_dates_and_cat(time.beginning_of_week, time.end_of_week,params[:category_id]).order(start_time: :asc)
+    @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time.beginning_of_week, time.end_of_week).order(start_time: :asc)
   end
 
   def year
     time = DateTime.new(params[:year])
-    @events = City.find_by_city(params[:city]).events.bw_dates_and_cat(time, time.end_of_year,params[:category_id]).order(start_time: :asc)
+    @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time, time.end_of_year).order(start_time: :asc)
   end
 
   def month
     time =DateTime.new(params[:year], params[:month])
-    @events = City.find_by_city(params[:city]).bw_dates_and_cat(time, time.end_of_month,params[:category_id]).order(start_time: :asc)
+    @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time, time.end_of_month).order(start_time: :asc)
   end
 
   def day
     time =DateTime.new(params[:year], params[:month],params[:day])
-    @events = City.find_by_city(params[:city]).events.bw_dates_and_cat(time, time.end_of_day,params[:category_id]).order(start_time: :asc)
+    @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time, time.end_of_day).order(start_time: :asc)
   end
 
   def week
     time = DateTime.commercial(DateTime.now.year,params[:week].to_i)
-    @events = City.find_by_city(params[:city]).events.bw_dates_and_cat(time, time.end_of_week,params[:category_id]).order(start_time: :asc)
+    @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time, time.end_of_week).order(start_time: :asc)
   end
 
   def topyear
     time = DateTime.new(params[:year])
-    @events = City.find_by_city(params[:city]).events.bw_dates_and_cat(time, time.end_of_year).order(total_stars: :desc)
+    @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time, time.end_of_year).order(total_stars: :desc)
   end
 
   def topmonth
     time = DateTime.new(params[:year], params[:month])
-    @events = City.find_by_city(params[:city]).events.bw_dates_and_cat(time, time.end_of_month).order(total_stars: :desc)
+    @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time, time.end_of_month).order(total_stars: :desc)
   end
 
 end

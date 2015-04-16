688c63c96dfaeb586aa927e4d8e52014d445bf4a
Usman Anwar <uanwar88@gmail.com>
2015-04-16 10:34:17 -0500

    Added city category routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    # This retrieves the events for a given category in a given city for the current week
     time = DateTime.now
     @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time.beginning_of_week, time.end_of_week).order(start_time: :asc)
   end
@@ -26,6 +27,7 @@ class CategoriesController < ApplicationController
   end
 
   def week
+    # This allows you to find events for a selected week
     time = DateTime.commercial(DateTime.now.year,params[:week].to_i)
     @events = City.find_by_city(params[:city]).events.category(params[:category_id].between_dates(time, time.end_of_week).order(start_time: :asc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -42,61 +42,12 @@ Rails.application.routes.draw do
   end
 
   scope path: ":city/categories", controller: :categories do
-    get "/:category_id" => "categories#show"
+    get "/:category_id" => :show
+    get "/:category_id/:year" => :year
+    get "/:category_id/:year/:month" => :month
+    get "/:category_id/:year/:month/:day" => :day
+    get "/:category_id/:year/top" => :topyear
+    get "/:category_id/:year/:month/top" => :topmonth
+    get "/:category_id/:year/week/:week" => :week
   end
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
